ec9ad632b6141703df55b439727a8882a25dbeae
ossy vincent <codingossy@gmail.com>
2023-02-08 19:45:29 +0100

    new update

--- zustand/store.jsx
+++ zustand/store.jsx
@@ -1,27 +1,29 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useStore = create((set) => ({
-  // cart
-  cart: {
-    foods: [],
-  },
-
-  // add to cart
-  addFoodToCart: (data) =>
-    set((state) => ({
-      cart: {
-        foods: [...state.cart.foods, data],
-      },
-    })),
-
-  // comot from cart
-  comotFoodFromCart: (index) =>
-    set((state) => ({
-      cart: {
-        foods: state.cart.foods.filter((_, i) => i !=index),
-      },
-    })),
-
+export const useStore = create(persist(
+  (set) => ({
+    // cart
+    cart: {
+      foods: [],
+    },
+  
+    // add to cart
+    addFoodToCart: (data) =>
+      set((state) => ({
+        cart: {
+          foods: [...state.cart.foods, data],
+        },
+      })),
+  
+    // comot from cart
+    comotFoodFromCart: (index) =>
+      set((state) => ({
+        cart: {
+          foods: state.cart.foods.filter((_, i) => i != index),
+        },
+      })),
+  
     // clear cart
     // clearCart: (index) =>
     // set((state) => ({
@@ -29,15 +31,15 @@ export const useStore = create((set) => ({
     //     foods: state.cart.foods.filter((_, index) => index !=index),
     //   },
     // })),
-
+  
     // to clear all cart
-    clearCart: () => 
-    set(() => ({
-      cart: {
-        foods: []
-      }
-    }))
-    
-
-
-}));
+    clearCart: () =>
+      set(() => ({
+        cart: {
+          foods: [],
+        },
+      })),
+  }),{
+    name: 'cart'
+  }
+));
